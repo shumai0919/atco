3865f421236b39000f09c8f820f61c284b0b9c03
shumai0919 <gamityhimiyyru@gmail.com>
2026-03-14 23:17:53 +0900

    abc449

---
+++ abc/449/a.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pi numbers::pi
+
+int main(void) {
+    double d;
+    cin >> d;
+    double r = d / 2;
+    double a = (r *r) * pi;
+    cout << fixed << setprecision(6);
+    cout << a << endl;
+}
---
+++ abc/449/b.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pi numbers::pi
+
+int main(void) {
+    
+}
